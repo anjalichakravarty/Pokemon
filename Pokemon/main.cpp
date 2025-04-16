1d7aadafcb70c4e4f692df4170285ae0e1361128
anjalichakravarty <anjalichakravarty9@gmail.com>
2025-04-16 22:11:10 +0530

    Update main.cpp

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -2,14 +2,26 @@
 using namespace std;
 
 int main() {
+    
+    //variables 
+    string player_name;
 
-    //start the game
-    cout << "Welcome to the world of Pokémon! I am Professor Oak.\n";
+    //introduction by professor
+    cout << "Professor Oak: Welcome to the world of Pokémon! I am Professor Oak.\n";
+    cout << "Professor Oak: Today is a momentous day—you’ll be choosing your very first Pokémon.";
+
+    //taking player name as input
+    cout << "Professor Oak: But before you embark on your journey, tell us your name!";
+    cin >> player_name;
+    cout << "Professor Oak: Ah," << player_name <<"! What a great name!\n"<<endl;
+
+    //choosing the pokemon
+    cout << "Professor Oak: I have three Pokémon with me";
     cout << "You can choose one of the following Pokémon:\n";
     cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
-    cout << "Which Pokémon would you like to choose? Enter the number: ";
 
     int choice;
+    cout << "Which Pokémon would you like to choose? Enter the number: ";
     cin >> choice;
 
     if (choice == 1)
@@ -29,16 +41,10 @@ int main() {
         cout << "Invalid choice. Please restart the game.\n";
     }
     
+
+
+
     
-
-    //player name
-    string player_name;
-    cout << "Trainer! Before you embark on your journey, \n the Pokémon world needs to know who you are!";
-    cout << "Time to step up and tell us your name. \n And remember, a great name is the start of every great adventure!";
-    cin >> player_name;
-
-    cout << "Great start Trainer!" << player_name <<"Looks like you’ve mastered the opening of every code adventure. \n Keep this up, and you’ll be a coding master in no time!"<<endl;
-
 
 
     return 0;
