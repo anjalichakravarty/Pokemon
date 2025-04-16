26a7c4f5238a5641cf3a483aeda01d79f645b7ff
anjalichakravarty <anjalichakravarty9@gmail.com>
2025-04-16 21:33:38 +0530

    Update main.cpp

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -4,11 +4,12 @@ using namespace std;
 int main() {
 
     string player_name;
-    cout << "Enter your name";
+    cout << "Trainer! Before you embark on your journey, \n the Pokémon world needs to know who you are!";
+    cout << "Time to step up and tell us your name. \n And remember, a great name is the start of every great adventure!";
     cin >> player_name;
 
-    cout << "Great Start" << player_name <<", looks like you have understood the main() function properly now"<<endl;
-    
+    cout << "Great start Trainer!" << player_name <<"Looks like you’ve mastered the opening of every code adventure. \n Keep this up, and you’ll be a coding master in no time!"<<endl;
+
 
 
     return 0;
