2fe9c7504cf00c540432335f3b07e50a8ccdf766
anjalichakravarty <anjalichakravarty9@gmail.com>
2025-04-17 12:52:11 +0530

    Update main.cpp

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 using namespace std;
 
+//Define an enum of pokemon choices
+enum PokemonChoice {
+    Charmander,
+    Bulbasaur,
+    Squirtle,
+    InvalidChoice
+};
+
 int main() {
     
     //variables 
     string player_name;
-    string chosen_pokemon;
+    PokemonChoice chosen_pokemon = InvalidChoice; //default to an invalid choice
 
     //introduction by professor
     cout << "Professor Oak: Welcome to the world of Pokémon! I am Professor Oak.\n";
@@ -28,32 +36,26 @@ int main() {
     switch (choice)
     {
     case 1:
-        chosen_pokemon = "Charmander";
+        chosen_pokemon = Charmander;
         cout << "Professor Oak: A fiery choice! Charmander is yours!\n";
         break;
 
     case 2:
-        chosen_pokemon = "Bulbasaur";
+        chosen_pokemon = Bulbasaur;
         cout << "Professor Oak: A fine choice! Bulbasaur is always ready to grow on you!\n";
         break;
 
     case 3:
-        chosen_pokemon = "Squirtle";
+        chosen_pokemon = Squirtle;
         cout << "Professor Oak: Splendid! Squirtle will keep you cool under pressure!\n";
         break;
     
     default:
         cout << "Professor Oak: Hmm, that doesn't seem right. Let me choose for you...\n";
-        chosen_pokemon = "Pikachu"; //Default if no vaild choice is made
-        cout << "Professor Oak: Just kidding! Let's go with Pikachu, the surprise guest.\n";
+        chosen_pokemon = Charmander; //Default if no vaild choice is made
+        cout << "Professor Oak: Just kidding! Let's go with Charmander, the fiery dragon in the making!\n";
         break;
     }
     
-
-
-
-    
-
-
     return 0;
 }
